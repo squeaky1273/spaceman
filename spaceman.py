7373ff6184e2dcbb2c68e7253c0658c6ef1f1ac4
squealy1273 <padynriddell@Padyns-MacBook-Pro.local>
2019-09-05 15:44:12 -0700

    Removed word_list[0]

--- spaceman.py
+++ spaceman.py
@@ -1,7 +1,12 @@
 import random
 
 def load_word():
-
+    '''
+    A function that reads a text file of words and randomly selects one to use as the secret word
+        from the list.
+    Returns:
+           string: The secret word to be used in the spaceman guessing game
+    '''
     f = open('words.txt', 'r')
     words_list = f.readlines()
     f.close()
@@ -9,17 +14,47 @@ def load_word():
     words_list = words_list[0].split(' ')
     secret_word = random.choice(words_list)
     return secret_word
+    pass
 
 def is_word_guessed(secret_word, letters_guessed):
+    '''
+    A function that checks if all the letters of the secret word have been guessed.
+    Args:
+        secret_word (string): the random word the user is trying to guess.
+        letters_guessed (list of strings): list of letters that have been guessed so far.
+    Returns:
+        bool: True only if all the letters of secret_word are in letters_guessed, False otherwise
+    '''
     pass
 
 def get_guessed_word(secret_word, letter_guessed):
+    '''
+    A function that is used to get a string showing the letters guessed so far in the secret word and underscores for letters that have not been guessed yet.
+    Args:
+        secret_word (string): the random word the user is trying to guess.
+        letters_guessed (list of strings): list of letters that have been guessed so far.
+    Returns:
+        string: letters and underscores.  For letters in the word that the user has guessed correctly, the string should contain the letter at the correct position.  For letters in the word that the user has not yet guessed, shown an _ (underscore) instead.
+    '''
     pass
 
 def is_guess_in_word(guess, secret_word):
+    '''
+    A function to check if the guessed letter is in the secret word
+    Args:
+        guess (string): The letter the player guessed this round
+        secret_word (string): The secret word
+    Returns:
+        bool: True if the guess is in the secret_word, False otherwise
+    '''
     pass
 
 def spaceman(secret_word):
+    '''
+    A function that controls the game of spaceman. Will start spaceman in the command line.
+    Args:
+      secret_word (string): the secret word to guess.
+    '''
     pass
 
 
